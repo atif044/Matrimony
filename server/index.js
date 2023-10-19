ed525aa8f39b063739aee3cb62b4c8aeedbb536e
atif044 <74341827+atif044@users.noreply.github.com>
2023-10-19 23:04:12 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app=express();
 const auth=require('./Routes/userRoutes')
 const adminAuth=require('./Routes/adminRoutes')
 app.use(express.json());
-app.use(cors({ origin: 'https://matrimony-nu.vercel.app', credentials: true }))
+app.use(cors({ origin: 'https://atif044.github.io', credentials: true }))
 app.options('*',cors())
 app.use(cookieParser())
 app.listen(5000)
