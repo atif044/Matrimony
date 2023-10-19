39e5fe9faea49065f30c61232b136544ff0cc9cd
atif044 <74341827+atif044@users.noreply.github.com>
2023-10-19 22:33:13 +0500

    Update userRoutes.js

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -68,27 +68,26 @@ router.post('/login',async(req,res)=>{
         
         res.cookie("icCompleted",isCompleted,{
             secure:true,
-            httpOnly:true,
             maxAge:24 * 60 * 60 * 1000,
             // secure:true,
-            // sameSite:'none',
+             sameSite:'none',
             expires:new Date(Date.now()+24 * 60 * 60 * 1000)
         })
         res.cookie("typeAdmin",isAdmin,{
             secure:true,
-            httpOnly:true,
+            
             maxAge:24 * 60 * 60 * 1000,
             // secure:true,
-            // sameSite:'none',
+            sameSite:'none',
             expires:new Date(Date.now()+24 * 60 * 60 * 1000)
         })
         return res.status(200).cookie("Authorization",`Bearer ${authToken}`,{
             
             secure:true,
-            httpOnly:true,
+            
             maxAge:24 * 60 * 60 * 1000,
             // secure:true,
-            // sameSite:'none',
+            sameSite:'none',
             expires:new Date(Date.now()+24 * 60 * 60 * 1000)
         }).json({success:true,msg:"You are logged in",Details:{id:user._id,Name,Email,DateofBirth,isAdmin}});
 })
